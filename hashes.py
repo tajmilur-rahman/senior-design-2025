44637121f6e9501c0f03ab0c6ba48a694fd0d477
Anunjin Batdelger <batdelge002@gannon.edu>
2025-11-17 09:26:38 -0500

    remove old files

--- hashes.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-admin_pw = "admin123"
-admin_hash = bcrypt.hashpw(admin_pw.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
-print("Admin hash:", admin_hash)
-
-student_pw = "gannon2025"
-student_hash = bcrypt.hashpw(student_pw.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
-print("Student hash:", student_hash)
